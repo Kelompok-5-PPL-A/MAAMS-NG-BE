f2416441f71604fafa7c7fa062d3330a365b0d13
Steven Faustin Orginata <stevenfo0711@gmail.com>
2025-03-04 23:34:04 +0700

    feat: [GREEN] implement id and status attr on Problem model

--- question/models.py
+++ question/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
+import uuid
 
 class Problem(models.Model):
+    STATUS_CHOICES = [
+        ('PRIBADI', 'Pribadi'),
+        ('PENGAWASAN', 'Pengawasan'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_email = models.EmailField()
     question = models.CharField(max_length=255, null=False)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='PRIBADI'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
