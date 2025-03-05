cc2338be459bf07620bad56dae6e86218c4791b3
ariananurlayla <ariana.nurlayla@ui.ac.id>
2025-03-05 23:29:06 +0700

    feat: [GREEN] implement question removal functionality

--- question/urls.py
+++ question/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('submit/', views.submit_question, name='submit_question'),
     path('success/', views.success, name='success'),  # A success page after form submission
+    path('remove/<uuid:question_id>/', views.remove_question, name='remove_question'),
+    path('remove-success/', views.remove_success, name='remove_success'),
 ]
--- question/views.py
+++ question/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.db import IntegrityError
 from .models import Problem
@@ -40,3 +40,14 @@ def submit_question(request):
 # Success page
 def success(request):
     return HttpResponse("Your question has been successfully submitted!")
+
+def remove_question(request, question_id):
+    """Remove a question based on its ID."""
+    problem = get_object_or_404(Problem, id=question_id)
+    problem.delete()
+    return redirect('remove_success')  # Redirect to the new remove success page
+
+def remove_success(request):
+    """Notify the user that the question has been successfully removed."""
+    return HttpResponse("The question has been successfully removed!")
+
