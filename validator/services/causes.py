434d0cbc01c646333b5888dc738169751333c3b9
hilaldfzn <hilalfauzan9@gmail.com>
2025-03-09 06:27:56 +0700

    [REFACTOR] improved error handling and clarified response parsing

--- validator/services/causes.py
+++ validator/services/causes.py
@@ -1,47 +1,54 @@
+from django.conf import settings
 from groq import Groq
+import requests
+from validator.constans import ErrorMsg
 from validator.dataclasses.create_cause import CreateCauseDataClass
+from validator.enums import ValidationType
 from validator.models import questions
 from validator.models.causes import Causes
 from validator.services import questions
-from requests.exceptions import RequestException
 from validator.exceptions import AIServiceErrorException
 import uuid
 
 class CausesService:
-    def api_call(self, system_message, user_prompt, validation_type):
+    def api_call(self, system_message: str, user_prompt: str, validation_type:ValidationType) -> int:
+        client = Groq(api_key=settings.GROQ_API_KEY)
+        
         try:
-            client = Groq()
-            
-            messages = [
-                {
-                    "role": "system",
-                    "content": system_message,
-                },
-                {
-                    "role": "user",
-                    "content": user_prompt
-                }
-            ]
-            
-            response = client.chat.completions.create(
-                messages=messages,
+            chat_completion = client.chat.completions.create(
+                messages=[
+                    {
+                        "role": "system",
+                        "content": system_message,
+                    },
+                    {
+                        "role": "user",
+                        "content": user_prompt
+                    }
+                ],
                 model="llama-3.3-70b-specdec",
                 temperature=0.1,
                 max_tokens=50,
                 seed=42
             )
-
-            content = response.choices[0].message.content.lower()
-
-            if 'true' in content:
+            
+            answer = chat_completion.choices[0].message.content
+        
+        except requests.exceptions.RequestException:
+            raise AIServiceErrorException(ErrorMsg.AI_SERVICE_ERROR)
+        
+        if validation_type in [ValidationType.NORMAL, ValidationType.ROOT]:
+            if answer.lower().__contains__('true'):
                 return 1
-            else:
+            elif answer.lower().__contains__('false'):
                 return 0
-                
-        except RequestException:
-            raise AIServiceErrorException("Failed to call the AI service.")
-        except Exception as e:
-            raise
+        else:
+            if answer.__contains__('1'):
+                return 1
+            elif answer.__contains__('2'):
+                return 2
+            elif answer.__contains__('3'):  
+                return 3
         
     def create(self, question_id: uuid, cause: str, row: int, column: int, mode: str) -> CreateCauseDataClass:
         cause = Causes.objects.create(
