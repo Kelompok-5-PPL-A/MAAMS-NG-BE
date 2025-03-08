037d87b3fae5e171ab3c3ef318b3ab0fdd7ad2de
hilaldfzn <hilalfauzan9@gmail.com>
2025-03-09 00:45:04 +0700

    feat: create custom exceptions for API request validation

---
+++ validator/exceptions.py
@@ -0,0 +1,29 @@
+from rest_framework import status
+from rest_framework.exceptions import APIException
+
+class EmptyTagException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+
+class InvalidFiltersException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+
+class InvalidTagException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+
+class InvalidTimeRangeRequestException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    
+class UniqueTagException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    
+class ValueNotUpdatedException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+
+class ForbiddenRequestException(APIException):
+    status_code = status.HTTP_403_FORBIDDEN
+
+class NotFoundRequestException(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+
+class AIServiceErrorException(APIException):
+    status_code = status.HTTP_503_SERVICE_UNAVAILABLE
