e81d6bdd1abd4108998301e17bf02b30f4531913
hilaldfzn <hilalfauzan9@gmail.com>
2025-03-09 06:10:06 +0700

    feat: [GREEN] implement API call to integrate with Groq AI API

--- validator/services/causes.py
+++ validator/services/causes.py
@@ -1,14 +1,48 @@
-from typing import List
-from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
+from groq import Groq
 from validator.dataclasses.create_cause import CreateCauseDataClass
 from validator.models import questions
 from validator.models.causes import Causes
 from validator.services import questions
+from requests.exceptions import RequestException
+from validator.exceptions import AIServiceErrorException
 import uuid
-import requests
 
 class CausesService:
+    def api_call(self, system_message, user_prompt, validation_type):
+        try:
+            client = Groq()
+            
+            messages = [
+                {
+                    "role": "system",
+                    "content": system_message,
+                },
+                {
+                    "role": "user",
+                    "content": user_prompt
+                }
+            ]
+            
+            response = client.chat.completions.create(
+                messages=messages,
+                model="llama-3.3-70b-specdec",
+                temperature=0.1,
+                max_tokens=50,
+                seed=42
+            )
+
+            content = response.choices[0].message.content.lower()
+
+            if 'true' in content:
+                return 1
+            else:
+                return 0
+                
+        except RequestException:
+            raise AIServiceErrorException("Failed to call the AI service.")
+        except Exception as e:
+            raise
+        
     def create(self, question_id: uuid, cause: str, row: int, column: int, mode: str) -> CreateCauseDataClass:
         cause = Causes.objects.create(
             problem=questions.Question.objects.get(pk=question_id),
