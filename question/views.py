5a03ead6b9c49f46e66fa073f1ef8ca5e7737911
Arya Lesmana <arya.lesmana21@ui.ac.id>
2025-03-18 00:04:01 +0700

    [REFACTOR] reworked the post method so it will return various response status

--- question/views.py
+++ question/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 from rest_framework.decorators import permission_classes
 from drf_spectacular.utils import extend_schema
-
+from question.models import Question
 from .services import QuestionService
 from .serializers import QuestionRequest, QuestionResponse
 
@@ -17,16 +17,19 @@ class QuestionPost(APIView):
         responses=QuestionResponse,
     )
     def post(self, request):
-        request_serializer = QuestionRequest(data=request.data)
-        request_serializer.is_valid(raise_exception=True)
+                request_serializer = QuestionRequest(data=request.data)
+                request_serializer.is_valid(raise_exception=True)
 
-        service_class = QuestionService()
-        question = service_class.create(
-            **request_serializer.validated_data
-        )
+                service_class = QuestionService()
+                question = service_class.create(
+                    **request_serializer.validated_data
+                )
+                response_serializer = QuestionResponse(question)
+                if (not question.title) or (not question.question) or (question.mode not in [Question.ModeChoices.PENGAWASAN, Question.ModeChoices.PRIBADI]):
+                    return Response(status=status.HTTP_400_BAD_REQUEST)
+                else:
+                    return Response(response_serializer.data, status=status.HTTP_201_CREATED)
 
-        response_serializer = QuestionResponse(question)
-        return Response(response_serializer.data, status=status.HTTP_201_CREATED)
 
 @permission_classes([])
 class QuestionGet(ViewSet):    
