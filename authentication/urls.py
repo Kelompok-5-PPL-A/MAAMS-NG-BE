514dcbc4e5a0086844ef73ee96d567f3a6c01a87
hilaldfzn <hilalfauzan9@gmail.com>
2025-03-29 04:28:26 +0700

    feat: add URL login/logout for SSO UI

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,13 +5,27 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-from authentication.views import google_login
+from authentication.views import (
+    google_login, SSOLoginView, SSOLogoutView,
+    UserProfileView, UpdateContactView
+)
 
 app_name = 'authentication'
 
 urlpatterns = [
+    # Google OAuth
     path('login-google/', google_login, name='google_login'),
+    
+    # SSO UI
+    path('login-sso/', SSOLoginView.as_view(), name='sso_login'),
+    path('logout-sso/', SSOLogoutView.as_view(), name='sso_logout'),
+    
+    # Token management
     path('logout/', TokenBlacklistView.as_view(), name='logout'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    
+    # User profile management
+    path('profile/', UserProfileView.as_view(), name='user_profile'),
+    path('update-contact/', UpdateContactView.as_view(), name='update_contact'),
 ]
