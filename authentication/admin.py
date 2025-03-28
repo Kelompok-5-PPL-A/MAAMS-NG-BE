0bb8ad4d1dd27216a25b5855c80aefc1699a7ba9
hilaldfzn <hilalfauzan9@gmail.com>
2025-03-29 03:04:55 +0700

    refactor: update CustomUserAdmin to include additional fields

--- authentication/admin.py
+++ authentication/admin.py
@@ -7,22 +7,22 @@ from authentication.models import CustomUser
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'google_id')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                      'groups', 'user_permissions')}),
+        (None, {'fields': ('email', 'username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'google_id', 'npm', 'angkatan', 'noWA')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'role',
+                                       'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
-
+    
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
+            'fields': ('email', 'username', 'password1', 'password2', 'role'),
         }),
     )
-
-    list_display = ('email', 'first_name', 'last_name', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('email', 'first_name', 'last_name')
+    
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'role')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups', 'role')
+    search_fields = ('email', 'username', 'first_name', 'last_name', 'npm')
     ordering = ('email',)
     readonly_fields = ('date_joined', 'last_login')
