14d95a194ec59dd68c6fba52e6b711c2c73c2dcd
Steven Faustin Orginata <stevenfo0711@gmail.com>
2025-04-07 12:43:23 +0700

    test: [RED] implement user attributes in question model

--- question/models.py
+++ question/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from tag.models import Tag
+from django.conf import settings
 
 class Question(models.Model):
     class Meta:
@@ -17,3 +18,10 @@ class Question(models.Model):
     mode = models.CharField(max_length=20, choices=ModeChoices.choices, default=ModeChoices.PRIBADI)
     created_at = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField(Tag)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='question',
+    )
