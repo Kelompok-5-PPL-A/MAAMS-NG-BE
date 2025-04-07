8dca7948bcd43daac0479b599bf0310fc54fdf32
Steven Faustin Orginata <stevenfo0711@gmail.com>
2025-04-07 15:56:56 +0700

    refactor: specify import paths

--- cause/views.py
+++ cause/views.py
@@ -1,8 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from .services import CausesService
-from .serializers import BaseCauses, CausesRequest, CausesResponse
+from cause.services import CausesService
+from cause.serializers import BaseCauses, CausesRequest, CausesResponse
 from rest_framework import status
 from drf_spectacular.utils import extend_schema
 from rest_framework.decorators import permission_classes
