145d144a87ea2ccae25f9b084eb01e66ea086ea8
Arya Lesmana <arya.lesmana21@ui.ac.id>
2025-04-12 21:50:12 +0700

    fix: change CORS_ALLOW_ALL_ORIGINS to True to fix test error

--- MAAMS_NG_BE/settings.py
+++ MAAMS_NG_BE/settings.py
@@ -141,12 +141,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOW_ALL_ORIGINS = False
-
-CORS_ALLOWED_ORIGINS = [ 
-    os.getenv("HOST_FE"),
-    "http://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
