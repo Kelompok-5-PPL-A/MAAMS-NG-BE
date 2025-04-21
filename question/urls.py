f21efcd03b94887da262bc0ab90d0a59193b5946
Steven Faustin Orginata <stevenfo0711@gmail.com>
2025-04-22 01:01:20 +0700

    test: [GREEN] implement urls for question delete

--- question/urls.py
+++ question/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from .views import (
-    QuestionPost, QuestionGet, QuestionGetRecent
+    QuestionPost, QuestionGet, QuestionGetRecent, QuestionDelete
 )
 
 urlpatterns = [
     path('submit/', QuestionPost.as_view(), name="create_question"),
     path('<uuid:pk>/', QuestionGet.as_view({'get': 'retrieve'}), name="get_question"),
     path('recent/', QuestionGetRecent.as_view(), name='recent-question'),
+    path('<uuid:pk>/delete/', QuestionDelete.as_view(), name="delete_question"),
 ]
