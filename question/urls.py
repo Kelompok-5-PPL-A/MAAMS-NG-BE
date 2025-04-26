829ac75ae1c2f8a6d270cd5334ccbd183e3106bf
Arya Lesmana <arya.lesmana21@ui.ac.id>
2025-04-26 09:30:35 +0700

    fix: change path for search history for more consisteny between modules

--- question/urls.py
+++ question/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('submit/', QuestionPost.as_view(), name="create_question"),
     path('<uuid:pk>/', QuestionGet.as_view({'get': 'retrieve'}), name="get_question"),
     path('recent/', QuestionGetRecent.as_view(), name='recent-question'),
-    path('search/', QuestionGetMatched.as_view(), name="get_matched"),
+    path('history/search/', QuestionGetMatched.as_view(), name="get_matched"),
     path('history/', QuestionGetAll.as_view(), name='get_all'),
     path('privileged/', QuestionGetPrivileged.as_view(), name='privileged-question'),
 ]
