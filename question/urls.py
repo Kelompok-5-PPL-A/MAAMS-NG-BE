dc0636752a29ead3b841906e2d7cde79fafaa623
Arya Lesmana <arya.lesmana21@ui.ac.id>
2025-04-26 10:50:13 +0700

    feat: implement url for filter field values

--- question/urls.py
+++ question/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
 from .views import (
-    QuestionGetMatched, QuestionPost, QuestionGet, QuestionGetRecent, QuestionGetPrivileged, QuestionGetAll
+    QuestionGetMatched, QuestionPost, QuestionGet, QuestionGetRecent, QuestionGetPrivileged, QuestionGetAll, QuestionGetFieldValues
 )
 
 urlpatterns = [
     path('submit/', QuestionPost.as_view(), name="create_question"),
     path('<uuid:pk>/', QuestionGet.as_view({'get': 'retrieve'}), name="get_question"),
     path('recent/', QuestionGetRecent.as_view(), name='recent-question'),
-    path('history/search/', QuestionGetMatched.as_view(), name="get_matched"),
     path('history/', QuestionGetAll.as_view(), name='get_all'),
+    path('history/search/', QuestionGetMatched.as_view(), name="get_matched"),
+    path('history/field-values/', QuestionGetFieldValues.as_view(), name='get_field_values'),
     path('privileged/', QuestionGetPrivileged.as_view(), name='privileged-question'),
 ]
