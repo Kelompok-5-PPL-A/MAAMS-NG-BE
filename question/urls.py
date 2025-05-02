1fe616331dafffdfb58d0726eff989df1f5f01fb
ryandhika_al <ryandhika.al@ui.ac.id>
2025-05-02 16:32:45 +0700

    chore: add url for question patch

--- question/urls.py
+++ question/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('history/field-values/', QuestionGetFieldValues.as_view(), name='get_field_values'),
     path('<uuid:pk>/delete/', QuestionDelete.as_view(), name="delete_question"),
     path('privileged/', QuestionGetPrivileged.as_view(), name='privileged-question'),
+    path('ubah/<uuid:pk>/', QuestionPatch.as_view({'patch': 'patch_mode'}), name="patch_mode_question"),
+    path('ubah/judul/<uuid:pk>/', QuestionPatch.as_view({'patch': 'patch_title'}), name="patch_title_question"),
+    path('ubah/tags/<uuid:pk>/', QuestionPatch.as_view({'patch': 'patch_tags'}), name="patch_tags_question"),
 ]
