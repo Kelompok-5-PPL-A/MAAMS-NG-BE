c5fc4ecee9356437fc363e9e131bf8b99e1b9aee
ryandhika_al <ryandhika.al@ui.ac.id>
2025-05-05 06:25:48 +0700

    [REFACTOR] change validator model

--- validator/services.py
+++ validator/services.py
@@ -34,7 +34,7 @@ class CausesService:
                         "content": user_prompt
                     }
                 ],
-                model="llama-3.1-8b-instant",
+                model="deepseek-r1-distill-llama-70b",
                 temperature=0.7,
                 max_completion_tokens=8192,
                 top_p=0.95,
