d4a44696b34c4052e05425c965910083a5d4f0d7
Steven Faustin Orginata <stevenfo0711@gmail.com>
2025-05-20 15:30:18 +0700

    fix(settings): change database settings

--- MAAMS_NG_BE/settings.py
+++ MAAMS_NG_BE/settings.py
@@ -105,11 +105,24 @@ WSGI_APPLICATION = 'MAAMS_NG_BE.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
+
+# Test database settings
+if 'test' in sys.argv:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+
+# Database versioning configuration
+DB_VERSION_TABLE = 'django_migrations'
+DB_VERSION_APP = 'MAAMS_NG_BE'
+DB_VERSION_NAME = 'version'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
